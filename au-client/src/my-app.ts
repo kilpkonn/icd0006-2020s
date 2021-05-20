db3ce5cc12d2d430d7ee4470edfc168ff3b44bcb
tavo <tavo.annus@gmail.com>
2021-05-20 19:53:11 +0300

    remove router

--- au-client/src/my-app.ts
+++ au-client/src/my-app.ts
@@ -2,14 +2,22 @@ import {ICustomElementViewModel} from "aurelia";
 import {Login} from "./views/auth/login";
 import {Register} from "./views/auth/register";
 import {CarsRouter} from "./views/car/cars-router";
+import {CarIndex} from "./views/car/car-index";
+import {CarDetails} from "./views/car/car-details";
+import {CarDelete} from "./views/car/car-delete";
+import {CarEdit} from "./views/car/car-edit";
+import {CarCreate} from "./views/car/car-create";
 
 
 export class MyApp implements ICustomElementViewModel {
     static routes = [
         {path: 'au/login', id: 'login', component: Login, title: 'Login'},
         {path: 'au/register', id: 'register', component: Register, title: 'Register'},
-        {path: 'au/cars', id: 'car', component: CarsRouter, title: 'Cars'},
-        // {path: 'cars/:id/edit', id: 'car', component: CarsEdit, title: 'Cars edit'},
-        // {path: 'cars/:id', id: 'car', component: CarsDetails, title: 'Cars details'},
+        // {path: 'au/cars', id: 'car', component: CarsRouter, title: 'Cars'},
+        {path: 'au/cars', id: 'index', component: CarIndex, title: 'Cars'},
+        {path: 'au/cars/:id', id: 'details', component: CarDetails, title: 'Details'},
+        {path: 'au/cars/:id/delete', id: 'delete', component: CarDelete, title: 'Delete'},
+        {path: 'au/cars/:id/edit', id: 'edit', component: CarEdit, title: 'Edit'},
+        {path: 'au/cars/create', id: 'create', component: CarCreate, title: 'Create'},
     ];
 }
