a5f452e9e83e4c65987005cd332883e20745ca80
tavo <tavo.annus@gmail.com>
2021-05-20 18:52:45 +0300

    wtf is dis

--- chucknorris/src/my-app.ts
+++ chucknorris/src/my-app.ts
@@ -5,13 +5,13 @@ import {Categories} from "./views/categories";
 export class MyApp implements ICustomElementViewModel {
   static routes = [
     {
-      path: '/au/home',
+      path: '/chucknorris/home',
       id: 'home',
       component: Home,
       title: 'Home'
     },
     {
-      path: '/au/categories/:category',
+      path: '/chucknorris/categories/:category',
       id: 'categories',
       component: Categories,
       title: 'Categories'
