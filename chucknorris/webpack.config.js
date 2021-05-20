f5efb19abe64f10584e774163d4c3c978773e2a2
tavo <tavo.annus@gmail.com>
2021-05-20 17:47:55 +0300

    asdas

--- chucknorris/webpack.config.js
+++ chucknorris/webpack.config.js
@@ -3,17 +3,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
-const cssLoader = 'css-loader';
-
-
-const postcssLoader = {
-  loader: 'postcss-loader',
-  options: {
-    postcssOptions: {
-      plugins: ['autoprefixer']
-    }
-  }
-};
+// const cssLoader = 'css-loader';
+//
+//
+// const postcssLoader = {
+//   loader: 'postcss-loader',
+//   options: {
+//     postcssOptions: {
+//       plugins: ['autoprefixer']
+//     }
+//   }
+// };
 
 module.exports = function(env, { analyze }) {
   const production = true;
@@ -44,7 +44,7 @@ module.exports = function(env, { analyze }) {
         { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
         { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff' } },
         { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'file-loader' },
-        { test: /\.css$/i, use: [ 'style-loader', cssLoader, postcssLoader ] },
+        // { test: /\.css$/i, use: [ 'style-loader', cssLoader, postcssLoader ] },
         { test: /\.ts$/i, use: ['ts-loader', '@aurelia/webpack-loader'], exclude: /node_modules/ },
         { test: /[/\\]src[/\\].+\.html$/i, use: '@aurelia/webpack-loader', exclude: /node_modules/ }
       ]
